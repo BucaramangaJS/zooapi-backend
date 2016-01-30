241481f25f1c1e84138d6e9ddb4c8b0569103749
Jose Daniel Paternina <josedaniel.paterninasoto@gmail.com>
2016-01-30 01:15:26 -0500

    api de animales

--- api/controllers/AnimalController.js
+++ api/controllers/AnimalController.js
@@ -6,6 +6,58 @@
  */
 
 module.exports = {
-	
-};
+	//'post /animal'               : 'AnimalController.addnew',
+	addnew: function(req,res){
+		Animal.add(req.body).then(function(r){
+			if(!r.succes){
+				res.jsonp(500,r);
+			}else{
+				res.jsonp(201,r);
+			}
+		});
+	},
 
+	//'post /animal/:uuid'         : 'AnimalController.edit',
+	edit: function(req,res){
+		Animal.edit(req.body,req.param('uuid')).then(function(r){
+			if(!r.succes){
+				res.jsonp(500,r);
+			}else{
+				res.jsonp(202,r);
+			}
+		});
+	},
+
+	//'get  /animal/delete/:uuid'  : 'AnimalController.inactivate',
+	inactivate: function(req,res){
+		Animal.edit({active:0},req.param('uuid')).then(function(r){
+			if(!r.succes){
+				res.jsonp(500,r);
+			}else{
+				res.jsonp(202,r);
+			}
+		});
+	},
+
+	//'get  /animales'             : 'AnimalController.viewall',
+	viewall: function(req,res){
+		Animal.search({active:1}).then(function(r){
+			if(!r.succes){
+				res.jsonp(500,r);
+			}else{
+				res.jsonp(202,r);
+			}
+		});
+	},
+
+	//'get  /animal/:uuid'         : 'AnimalController.profile',
+	profile: function(req,res){
+		Animal.profile({id:req.param('uuid')}).then(function(r){
+			if(!r.succes){
+				res.jsonp(500,r);
+			}else{
+				res.jsonp(202,r);
+			}
+		});
+	}
+};
--- api/models/Animal.js
+++ api/models/Animal.js
@@ -44,5 +44,102 @@ module.exports = {
       collection: 'transaccion',
 			via: 'animal'
     }
+  },
+
+  //create an uuid for the object before creating a new record
+  beforeCreate: function (values, cb) {
+      var uuid        = require('node-uuid');
+      values.id       = uuid.v4();
+      cb();
+  },
+
+  //make sure the uuid gets removed before update
+  beforeUpdate: function (values, cb) {
+      if(values.id){
+        delete values.id;
+      }
+      cb();
+  },
+
+  /*
+  'post /animal'               : 'AnimalController.addnew',
+  'post /animal/:uuid'         : 'AnimalController.edit',
+  'get  /animal/delete/:uuid'  : 'AnimalController.inactivate',
+  'get  /animales'             : 'AnimalController.viewall',
+  'get  /animal/:uuid'         : 'AnimalController.profile',
+  */
+
+  add: function(data){
+    //create
+    return Animal.create(data)
+    //populate
+    .then(function(e){
+      return Animal.findOne({id:e.id})
+      .populate('zoo')
+      .populate('transacciones',{active:1});
+    })
+    //response data
+    .then(function(e){
+      return {succes: true, data: e};
+    })
+
+    //error management
+    .catch(function(e){
+      console.log(e);
+      return {succes: false, data: 'Hubo un error creando el Animal'};
+    });
+  },
+
+  edit: function(data,id){
+      //update
+      return Animal.update({id:id},data)
+
+      //populate
+      .then(function(e){
+        return Animal.findOne({id:e[0].id})
+        .populate('zoo')
+        .populate('transacciones',{active:1});
+      })
+
+      //response data
+      .then(function(e){
+        return {succes: true, data: e};
+      })
+
+      //error management
+      .catch(function(e){
+        console.log(e);
+        return {succes: false, data: 'Hubo un error editando el Animal'};
+      });
+  },
+
+  search: function(data){
+    return Animal.find(data)
+    .populate('zoo')
+    .populate('transacciones',{active:1})
+
+    //response data
+    .then(function(e){
+      return {succes:true, data:e};
+    })
+    //error management
+    .catch(function(){
+      return {succes:false, data: 'Ups.. algo se rompió en la busqueda de animales.'};
+    });
+  },
+
+  profile: function(data){
+    return Animal.findOne(data)
+    .populate('zoo')
+    .populate('transacciones',{active:1})
+
+    //response data
+    .then(function(e){
+      return {succes:true, data:e};
+    })
+    //error management
+    .catch(function(){
+      return {succes:false, data: 'Ups.. algo se rompió en el perfil del Animal.'};
+    });
   }
 };
